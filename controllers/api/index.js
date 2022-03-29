8e66ba18b37d429781d30e08bc15dfbc67bf5261
Eric Francey <eric.g.francey@gmail.com>
2022-03-28 23:16:01 -0400

    Reverting

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const bookRoutes = require('./bookRoutes');
+const techRoutes = require('./techRoutes');
 const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-router.use('/books', bookRoutes);
+router.use('/techs', techRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
